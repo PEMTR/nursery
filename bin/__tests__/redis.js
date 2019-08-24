d4f04aff3d5cfb65e02f2807ac079783995de95d
Mr.Panda <xivistudios@gmail.com>
2019-08-24 17:35:18 +0800

    update;

--- bin/__tests__/redis.js
+++ bin/__tests__/redis.js
@@ -3,4 +3,14 @@
 
 // package
 // @package
-const redis = require("../redis")
+const path = require("path")
+const util = require("../util")
+const redix = require("../redis")
+const _file = "../../configure/.dev.toml"
+const _config = path.join(__dirname, _file)
+const configure = util.readtoml(_config)
+const redis = new redix({ configure })
+
+test("绑定事件", function () {
+    expect(1+1).toBe(2)
+})
